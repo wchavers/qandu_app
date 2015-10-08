f15a2723d926b74fbbfeaf42a9df88dee0344785
wchavers <wachavers@quinnipiac.edu>
2015-10-08 01:04:18 +0000

    Add answer delete view

--- core/urls.py
+++ core/urls.py
@@ -11,5 +11,6 @@ urlpatterns = patterns('',
      url(r'^question/update/(?P<pk>\d+)/$', QuestionUpdateView.as_view(), name ='question_update'),
      url(r'^question/delete/(?P<pk>\d+)/$', QuestionDeleteView.as_view(), name='question_delete'),    
      url(r'^question/(?P<pk>\d+)/answer/create/$', AnswerCreateView.as_view(), name='answer_create'), 
-     url(r'^question/(?P<question_pk>\d+)/answer/update/(?P<answer_pk>\d+) $', AnswerUpdateView.as_view(), name='answer_update'),                 
+     url(r'^question/(?P<question_pk>\d+)/answer/update/(?P<answer_pk>\d+) $', AnswerUpdateView.as_view(), name='answer_update'), 
+     url(r'^question/(?P<question_pk>\d+)/answer/delete/(?P<answer_pk>\d+) $', AnswerDeleteView.as_view(), name='answer_delete'),                     
 )                      
